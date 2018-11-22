ffa7f7b2dab1a82249a7f94d50886d04b8d8d15e
Sam Potter <spotter1642@gmail.com>
2018-11-21 19:12:38 -0600

    Adding header file for testing fucntions

---
+++ utilities.h
@@ -0,0 +1,14 @@
+/* Practice header file */
+
+#include <stdio.h>
+#include <gryv.h>
+
+#ifndef MYTOOLS_H
+#define MYTOOLS_H
+
+/* Function prototypes */
+
+int parse(int *var1, double *var2, char *var3);
+int foobar(int *var1, double *var2, char *var3);
+
+#endif
