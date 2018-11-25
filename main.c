161526304b242fc39fb86d9a68dacc2462442166
Sam Potter <spotter1642@gmail.com>
2018-11-25 17:49:10 -0600

    Adding print spacing

--- main.c
+++ main.c
@@ -18,13 +18,13 @@ int main(){
 	char *charfoo;
 	
 	grvy_input_fread_int("intfoo", &intfoo);
-	printf("value of infoo: %d", intfoo);
+	printf("value of infoo: %d\n", intfoo);
 
 	grvy_input_fread_double("doublefoo", &doublefoo);
-	printf("value of doublefoo: %f", doublefoo);
+	printf("value of doublefoo: %f\n", doublefoo);
 	
 	grvy_input_fread_char("charfoo", &charfoo);
-	printf("value of charfoo: %s", charfoo);
+	printf("value of charfoo: %s\n", charfoo);
 
 	return 0;
 
